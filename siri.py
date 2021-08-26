2647e3cfb37a5ebb046cfcf78e27dfd2ccd82675
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-08-26 09:32:01 +0530

    Update siri.py

--- siri.py
+++ siri.py
@@ -88,7 +88,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>Title:</b> <ahref="{link}" >{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>Uploaded By:</b> <a href="https://https://t.me/storytym">StM Song Bot</a>'
+        rep = f'🎶 <b>Title:</b> <ahref="{link}" >{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>Uploaded By:</b> <a href="https://https://t.me/VAMPIRE_KING_NO_1">ᗪᗩᖇK ᒪOᖇᗪ</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
